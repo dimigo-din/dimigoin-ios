33296029b74f5a3834b4432c57503bb45a43a0d8
ByunKyungMin <bkm.change.min@gmail.com>
2020-07-04 08:21:43 +0900

    Prevent 'Landscape mode'

--- dimigoin/Application/AppDelegate.swift
+++ dimigoin/Application/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -32,6 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    static var orientationLock = UIInterfaceOrientationMask.portrait
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
 }
 
