bd634aac4ba6338124bebab4be4c51865124ea99
Changemin <bkm.change.min@gmail.com>
2020-10-16 11:03:18 +0900

    Add 'NavigationViewStyle Modifier for 'iPad'

--- dimigoin/Components/IngangItem.swift
+++ dimigoin/Components/IngangItem.swift
@@ -15,73 +15,69 @@ struct IngangItem: View {
     
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
-                HStack(alignment: .bottom) {
-                    Text(self.ingang.title).highlight().headline()
-                    Text(ingangTime[self.ingang.time]).caption2()
-
-                }
+            VStack(alignment: .leading){
+                Text(self.ingang.title).highlight().headline()
                 VSpacer(10)
-                HStack {
-                    Text("현원 \(self.ingang.present)명 / 총원 \(self.ingang.max_user)명").body()
-                    Spacer()
-                    if !self.ingang.status {
-                        Button(action: {
-                            self.ingang.status.toggle()
-                            let ingangStatus = ingangAPI.applyIngang(idx: ingang.idx)
-                            var message = ""
-                            switch ingangStatus {
-                                case .success: message = "인강실 신청 완료"
-                                case .usedAllTicket: message = "오늘(일주일)치 신청을 모두 했습니다"
-                                case .noIngang: message = "인강실 신청이 없습니다"
-                                case .timeout: message = "신청 시간이 아닙니다"
-                                case .blacklisted: message = "인강실 블랙리스트이므로 신청할 수 없습니다"
-                                case .full: message = "이미 신청을 했거나 신청인원이 꽉 찼습니다"
-                            }
-                            print(message)
-                            if(ingangStatus != .success) {
-                                SPAlert.present(title: "신청 실패", message: message, preset: .privacy)
-                                self.ingang.status.toggle()
-                            }
-                            else {
-                                SPAlert.present(title: "신청 완료", preset: .done)
-                            }
-                            ingangAPI.getIngangList()
-                            ingangAPI.getApplicantList()
-                            ingangAPI.getTickets()
-                        }) {
-                            Text("신청하기").body()
-                        }
-                    } else {
-                        Button(action: {
-                            self.ingang.status.toggle()
-                            let ingangStatus = ingangAPI.cancelIngang(idx: ingang.idx)
-                            var message = ""
-                            switch ingangStatus {
-                                case .success: message = "인강실 신청 완료"
-                                case .usedAllTicket: message = "반 또는 학년이 맞지 않습니다"
-                                case .noIngang: message = "인강실 또는 신청이 없습니다"
-                                case .timeout: message = "신청 시간이 아닙니다"
-                                case .blacklisted: message = ""
-                                case .full: message = ""
-                            }
-                            print(message)
-                            if(ingangStatus != .success) {
-                                SPAlert.present(title: "인강 취소 실패", message: message, preset: .privacy)
-                                self.ingang.status.toggle()
-                            }
-                            else {
-                                SPAlert.present(title: "인강 취소 완료", preset: .done)
-                            }
-                            ingangAPI.getIngangList()
-                            ingangAPI.getApplicantList()
-                            ingangAPI.getTickets()
-                        }) {
-                            Text("취소하기")
-                                .foregroundColor(Color("DisabledButton"))
-                        }
+                Text("현원 \(self.ingang.present)명 / 총원 \(self.ingang.max_user)명").body()
+            }
+            
+            Spacer()
+            if !self.ingang.status {
+                Button(action: {
+                    self.ingang.status.toggle()
+                    let ingangStatus = ingangAPI.applyIngang(idx: ingang.idx)
+                    var message = ""
+                    switch ingangStatus {
+                        case .success: message = "인강실 신청 완료"
+                        case .usedAllTicket: message = "오늘(일주일)치 신청을 모두 했습니다"
+                        case .noIngang: message = "인강실 신청이 없습니다"
+                        case .timeout: message = "신청 시간이 아닙니다"
+                        case .blacklisted: message = "인강실 블랙리스트이므로 신청할 수 없습니다"
+                        case .full: message = "이미 신청을 했거나 신청인원이 꽉 찼습니다"
                     }
+                    print(message)
+                    if(ingangStatus != .success) {
+                        SPAlert.present(title: "신청 실패", message: message, preset: .privacy)
+                        self.ingang.status.toggle()
+                    }
+                    else {
+                        SPAlert.present(title: "신청 완료", preset: .done)
+                    }
+                    ingangAPI.getIngangList()
+                    ingangAPI.getApplicantList()
+                    ingangAPI.getTickets()
+                }) {
+                    Text("신청하기").body()
                 }
+            } else {
+                Button(action: {
+                    self.ingang.status.toggle()
+                    let ingangStatus = ingangAPI.cancelIngang(idx: ingang.idx)
+                    var message = ""
+                    switch ingangStatus {
+                        case .success: message = "인강실 신청 완료"
+                        case .usedAllTicket: message = "반 또는 학년이 맞지 않습니다"
+                        case .noIngang: message = "인강실 또는 신청이 없습니다"
+                        case .timeout: message = "신청 시간이 아닙니다"
+                        case .blacklisted: message = ""
+                        case .full: message = ""
+                    }
+                    print(message)
+                    if(ingangStatus != .success) {
+                        SPAlert.present(title: "인강 취소 실패", message: message, preset: .privacy)
+                        self.ingang.status.toggle()
+                    }
+                    else {
+                        SPAlert.present(title: "인강 취소 완료", preset: .done)
+                    }
+                    ingangAPI.getIngangList()
+                    ingangAPI.getApplicantList()
+                    ingangAPI.getTickets()
+                }) {
+                    Text("취소하기")
+                        .foregroundColor(Color("Secondary"))
+                }
+                    
             }
         }.CustomBox()
     }
--- dimigoin/Views/MainView.swift
+++ dimigoin/Views/MainView.swift
@@ -46,6 +46,7 @@ struct MainView: View {
                     }
             )
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
