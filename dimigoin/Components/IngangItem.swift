5ac0ac904859f68229528cc41ed20b3dc81812e9
Changemin <bkm.change.min@gmail.com>
2020-10-18 00:59:51 +0900

    Add 'Checkmark' to 'IngangItem'

--- dimigoin/Components/IngangItem.swift
+++ dimigoin/Components/IngangItem.swift
@@ -16,94 +16,103 @@ struct IngangItem: View {
     @State var ingang: Ingang
     
     var body: some View {
-        HStack {
-            VStack(alignment: .leading){
-                Text(self.ingang.title).highlight().headline()
-                VSpacer(10)
-                Text("현원 \(self.ingang.present)명 / 총원 \(self.ingang.max_user)명").body()
-            }
-            Spacer()
-            if !self.ingang.status {
-                Button(action: {
-                    print("apply ingang : \(ingang.idx)")
-                    let headers: HTTPHeaders = [
-                        "Authorization":"Bearer \(tokenAPI.tokens.token)"
-                    ]
-                    let parameters: [String: String] = [
-                        "ingang_idx": "\(String(ingang.idx))"
-                    ]
-                    let url = "https://api.dimigo.in/ingang/"
-                    AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
-                        if let status = response.response?.statusCode {
-                            switch(status) {
-                            case 200: //success
-                                SPAlert.present(title: "인강 신청 성공",message: "인강이 성공적으로\n신청되었습니다.", preset: .done)
-                                self.ingang.status.toggle()
-                            case 403: // 본인 학년&반 인강실이 아니거나 오늘(일주일)치 신청을 모두 했습니다.
-                                SPAlert.present(title: "인강 신청 실패", message: "본인 학년&반 인강실이 아니거나\n오늘(일주일)치 신청을 모두 했습니다.", preset: .privacy)
-                            case 404: //인강실 신청이 없습니다.
-                                SPAlert.present(title: "인강 신청 실패", message: "인강실 신청이 없습니다.", preset: .privacy)
-                            case 405: // 신청 시간이 아닙니다
-                                SPAlert.present(title: "인강 신청 실패", message: "신청 시간이 아닙니다.\n신청 시간 : 7:00~8:15", preset: .privacy)
-                            case 406: // 인강실 블랙리스트이므로 신청할 수 없습니다.
-                                SPAlert.present(title: "인강 신청 실패", message: "인강실 블랙리스트이므로\n신청할 수 없습니다.", preset: .privacy)
-                            case 409: // 이미 신청을 했거나 신청인원이 꽉 찼습니다.
-                                SPAlert.present(title: "인강 신청 실패", message: "이미 신청을 했거나\n신청인원이 꽉 찼습니다.", preset: .privacy)
-                            case 500: // some error occured
-                                SPAlert.present(title: "Error(500)", message: "디미고인 시스템이\n망가진거 같아요", preset: .exclamation)
-                            default:
-                                self.tokenAPI.refreshTokens()
-                                debugPrint(response)
-                            }
-                            ingangAPI.getTickets()
-                            ingangAPI.getIngangList()
-                            ingangAPI.getApplicantList()
-                        }
-                    }
-                }) {
-                    Text("신청하기").body()
+        ZStack {
+            
+            HStack {
+                VStack(alignment: .leading){
+                    Text(self.ingang.title).highlight().headline()
+                    VSpacer(10)
+                    Text("현원 \(self.ingang.present)명 / 총원 \(self.ingang.max_user)명").body()
                 }
-            } else {
-                Button(action: {
-                    print("cancel ingang : \(ingang.idx)")
-                    let headers: HTTPHeaders = [
-                        "Authorization":"Bearer \(tokenAPI.tokens.token)"
-                    ]
-                    let parameters: [String: String] = [
-                        "ingang_idx": "\(String(ingang.idx))"
-                    ]
-                    let url = "https://api.dimigo.in/ingang/\(String(ingang.idx))/"
-                    AF.request(url, method: .delete, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
-                        if let status = response.response?.statusCode {
-                            switch(status) {
-                            case 200: //success
-                                SPAlert.present(title: "인강 취소 성공",message: "인강이 성공적으로\n취소되었습니다.", preset: .error)
-                                self.ingang.status.toggle()
-                            case 403: // 본인 학년&반 인강실이 아니거나 오늘(일주일)치 신청을 모두 했습니다.
-                                SPAlert.present(title: "인강 취소 실패",message: "본인 학년&반 인강실이 아니거나 오늘(일주일)치 신청을 모두 했습니다.", preset: .privacy)
-                            case 404: //인강실 신청이 없습니다.
-                                SPAlert.present(title: "인강 취소 실패",message: "인강실 신청이 없습니다.", preset: .privacy)
-                            case 405: // 신청 시간이 아닙니다
-                                SPAlert.present(title: "인강 취소 실패",message: "신청 시간이 아닙니다.\n신청 시간 : 7:00~8:15", preset: .privacy)
-                            case 500: // some error occured
-                                SPAlert.present(title: "Error(500)", message: "디미고인 시스템이 망가진거 같아요", preset: .exclamation)
-                            default:
-                                self.tokenAPI.refreshTokens()
-                                debugPrint(response)
+                Spacer()
+                if !self.ingang.status {
+                    Button(action: {
+                        print("apply ingang : \(ingang.idx)")
+                        let headers: HTTPHeaders = [
+                            "Authorization":"Bearer \(tokenAPI.tokens.token)"
+                        ]
+                        let parameters: [String: String] = [
+                            "ingang_idx": "\(String(ingang.idx))"
+                        ]
+                        let url = "https://api.dimigo.in/ingang/"
+                        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
+                            if let status = response.response?.statusCode {
+                                switch(status) {
+                                case 200: //success
+                                    SPAlert.present(title: "인강 신청 성공",message: "인강이 성공적으로\n신청되었습니다.", preset: .done)
+                                    self.ingang.status.toggle()
+                                case 403: // 본인 학년&반 인강실이 아니거나 오늘(일주일)치 신청을 모두 했습니다.
+                                    SPAlert.present(title: "인강 신청 실패", message: "본인 학년&반 인강실이 아니거나\n오늘(일주일)치 신청을 모두 했습니다.", preset: .privacy)
+                                case 404: //인강실 신청이 없습니다.
+                                    SPAlert.present(title: "인강 신청 실패", message: "인강실 신청이 없습니다.", preset: .privacy)
+                                case 405: // 신청 시간이 아닙니다
+                                    SPAlert.present(title: "인강 신청 실패", message: "신청 시간이 아닙니다.\n신청 시간 : 7:00~8:15", preset: .privacy)
+                                case 406: // 인강실 블랙리스트이므로 신청할 수 없습니다.
+                                    SPAlert.present(title: "인강 신청 실패", message: "인강실 블랙리스트이므로\n신청할 수 없습니다.", preset: .privacy)
+                                case 409: // 이미 신청을 했거나 신청인원이 꽉 찼습니다.
+                                    SPAlert.present(title: "인강 신청 실패", message: "이미 신청을 했거나\n신청인원이 꽉 찼습니다.", preset: .privacy)
+                                case 500: // some error occured
+                                    SPAlert.present(title: "Error(500)", message: "디미고인 시스템이\n망가진거 같아요", preset: .exclamation)
+                                default:
+                                    self.tokenAPI.refreshTokens()
+                                    debugPrint(response)
+                                }
+                                ingangAPI.getTickets()
+                                ingangAPI.getIngangList()
+                                ingangAPI.getApplicantList()
                             }
-                            ingangAPI.getTickets()
-                            ingangAPI.getIngangList()
-                            ingangAPI.getApplicantList()
                         }
+                    }) {
+                        Text("신청하기").body()
                     }
-                }) {
-                    Text("취소하기")
-                        .foregroundColor(Color("Secondary"))
-                        .body()
+                } else {
+                    Button(action: {
+                        print("cancel ingang : \(ingang.idx)")
+                        let headers: HTTPHeaders = [
+                            "Authorization":"Bearer \(tokenAPI.tokens.token)"
+                        ]
+                        let parameters: [String: String] = [
+                            "ingang_idx": "\(String(ingang.idx))"
+                        ]
+                        let url = "https://api.dimigo.in/ingang/\(String(ingang.idx))/"
+                        AF.request(url, method: .delete, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
+                            if let status = response.response?.statusCode {
+                                switch(status) {
+                                case 200: //success
+                                    SPAlert.present(title: "인강 취소 성공",message: "인강이 성공적으로\n취소되었습니다.", preset: .error)
+                                    self.ingang.status.toggle()
+                                case 403: // 본인 학년&반 인강실이 아니거나 오늘(일주일)치 신청을 모두 했습니다.
+                                    SPAlert.present(title: "인강 취소 실패",message: "본인 학년&반 인강실이 아니거나 오늘(일주일)치 신청을 모두 했습니다.", preset: .privacy)
+                                case 404: //인강실 신청이 없습니다.
+                                    SPAlert.present(title: "인강 취소 실패",message: "인강실 신청이 없습니다.", preset: .privacy)
+                                case 405: // 신청 시간이 아닙니다
+                                    SPAlert.present(title: "인강 취소 실패",message: "신청 시간이 아닙니다.\n신청 시간 : 7:00~8:15", preset: .privacy)
+                                case 500: // some error occured
+                                    SPAlert.present(title: "Error(500)", message: "디미고인 시스템이 망가진거 같아요", preset: .exclamation)
+                                default:
+                                    self.tokenAPI.refreshTokens()
+                                    debugPrint(response)
+                                }
+                                ingangAPI.getTickets()
+                                ingangAPI.getIngangList()
+                                ingangAPI.getApplicantList()
+                            }
+                        }
+                    }) {
+                        Text("취소하기")
+                            .foregroundColor(Color("Secondary"))
+                            .body()
+                    }
+                        
                 }
-                    
+            }.CustomBox()
+            if(ingang.status) {
+                Image(systemName: "checkmark.circle.fill")
+                    .font(Font.system(size: 20))
+                    .foregroundColor(Color.green)
+                    .offset(x: -(UIScreen.screenWidth/2-20), y: -35)
             }
-        }.CustomBox()
+        }
     }
 }
 
--- dimigoin/Views/IngangListView.swift
+++ dimigoin/Views/IngangListView.swift
@@ -15,13 +15,8 @@ struct IngangListView: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 15.0) {
-                VStack(alignment: .leading, spacing: 10.0) {
-                    Text("신청 안내").headline()
-                    Text("인강실에서는 인터넷 강의, 교과 과제, 프로그래밍 등 학습 관련 활동만 가능합니다.").caption2()
-                    Text("쾌적한 인터넷 환경을 위해 과제와 관련 없는 기타 활동은 금지됩니다. 와이파이는 모두가 이용하는 공공재입니다.").caption2()
-                    Text("신청 시간은 07:00 ~ 08:15입니다. 신청시간을 꼭 지켜주세요").caption2()
-                }
-                Divider()
+                
+//                Divider()
                 VStack(alignment: .leading, spacing: 10.0) {
                     Text("신청 정보").font(.headline)
                     HStack {
@@ -29,29 +24,29 @@ struct IngangListView: View {
                         Text("\(ingangAPI.daily_ticket_num - ingangAPI.weekly_request_count)개 / \(ingangAPI.weekly_ticket_num)개")
                     }.CustomBox()
                 }
-                Divider()
+//                Divider()
                 VStack(alignment: .leading, spacing: 10.0) {
                     Text("인강실 목록").font(.headline)
                     ForEach(ingangAPI.ingangs, id: \.self) { ingang in
                         IngangItem(ingangAPI: ingangAPI, tokenAPI: tokenAPI, ingang: ingang)
                     }
                 }
-                Divider()
+//                Divider()
                 VStack(alignment: .leading, spacing: 10.0) {
                     Text("우리반 신청자").font(.headline)
-                    HStack {
+                    HStack(alignment: .center) {
                         Text("1타임").highlight().headline()
                         ForEach(ingangAPI.applicants, id: \.self) { applicant in
-                            Text(applicant.name)
+                            Text(applicant.name).disabled().caption1()
                         }
                     }.CustomBox()
-                    
-                    HStack {
-                        Text("2타임").highlight().headline()
-                        ForEach(ingangAPI.applicants, id: \.self) { applicant in
-                            Text(applicant.name)
-                        }
-                    }.CustomBox()
+                }
+                Divider()
+                VStack(alignment: .leading, spacing: 10.0) {
+                    Text("신청 안내").font(.headline)
+                    Text("인강실에서는 인터넷 강의, 교과 과제, 프로그래밍 등 학습 관련 활동만 가능합니다.").caption2()
+                    Text("쾌적한 인터넷 환경을 위해 과제와 관련 없는 기타 활동은 금지됩니다. 와이파이는 모두가 이용하는 공공재입니다.").caption2()
+                    Text("신청 시간은 07:00 ~ 08:15입니다. 신청시간을 꼭 지켜주세요").caption2()
                 }
                 Spacer()
             }.padding()
