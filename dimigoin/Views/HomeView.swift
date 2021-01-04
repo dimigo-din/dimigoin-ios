0b4da9dbd3b1323441bb7acdc8d648e7b8bda075
Changemin <bkm.change.min@gmail.com>
2021-01-05 00:16:26 +0900

    Force 'local authentication' to open id card'

--- dimigoin/Views/HomeView.swift
+++ dimigoin/Views/HomeView.swift
@@ -34,8 +34,7 @@ struct HomeView: View {
                         Image("logo").resizable().aspectRatio(contentMode: .fit).frame(height: 38)
                         Spacer()
                         Button(action: {
-                            localAuthentication()
-//                            showIdCard()
+                            showIdCard()
                         }) {
                             // MARK: replace userPhoto-sample to userImage when backend ready
                             Image("userPhoto-sample")
@@ -59,78 +58,23 @@ struct HomeView: View {
         }
     }
     func showIdCard() {
-        withAnimation(.spring()) {
-            self.isShowIdCard = true
+        let scanner = LAContext()
+        scanner.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "학생증 보기") { (status, err) in
+            if err != nil {
+                print(err!.localizedDescription)
+                return
+            } else {
+               
+            }
+            withAnimation(.spring()) {
+                self.isShowIdCard = true
+            }
         }
     }
     func dismissIdCard() {
         withAnimation(.spring()) {
             self.isShowIdCard = false
         }
-    }
-    func localAuthentication() -> Void {
-        let authContext = LAContext()
-        
-        var error: NSError?
-        
-        var description: String!
-        
-        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            
-            switch authContext.biometryType {
-            case .faceID:
-                description = "계정 정보를 열람하기 위해서 Face ID로 인증 합니다."
-                break
-            case .touchID:
-                description = "계정 정보를 열람하기 위해서 Touch ID로 인증 사용합니다."
-                break
-            case .none:
-                description = "계정 정보를 열람하기 위해서는 로그인하십시오."
-                break
-            }
-            
-            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: description) { (success, error) in
-                
-                if success {
-                    print("인증 성공")
-                } else {
-                    print("인증 실패")
-                    if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-                
-            }
-            
-        }  else {
-            // Touch ID・Face ID를 사용할 수없는 경우
-            let errorDescription = error?.userInfo["NSLocalizedDescription"] ?? ""
-            print(errorDescription)
-            description = "계정 정보를 열람하기 위해서는 로그인하십시오."
-            
-            let alertController = UIAlertController(title: "Authentication Required", message: description, preferredStyle: .alert)
-            weak var usernameTextField: UITextField!
-            alertController.addTextField(configurationHandler: { textField in
-                textField.placeholder = "User ID"
-                usernameTextField = textField
-            })
-            weak var passwordTextField: UITextField!
-            alertController.addTextField(configurationHandler: { textField in
-                textField.placeholder = "Password"
-                textField.isSecureTextEntry = true
-                passwordTextField = textField
-            })
-            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-            alertController.addAction(UIAlertAction(title: "Log In", style: .destructive, handler: { action in
-                // 를
-                print(usernameTextField.text! + "\n" + passwordTextField.text!)
-            }))
-//            self.present(alertController, animated: true, completion: nil)
-        }
-        
-        
-
-
     }
 }
 
