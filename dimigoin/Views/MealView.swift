8d510cda7f176ca60a8220616b6ea5ca91a3190c
Changemin <bkm.change.min@gmail.com>
2021-01-06 02:17:18 +0900

    Fix 'MealItem overlap error'

--- dimigoin/Views/MealView.swift
+++ dimigoin/Views/MealView.swift
@@ -14,32 +14,53 @@ struct MealView: View {
     
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                ScrollView(showsIndicators: false) {
-                    HStack {
-                        ViewTitle("급식", sub: getDateString())
-                        Spacer()
-                        NavigationLink(destination: WeeklyMealView()) {
-                            Image("calender").resizable().aspectRatio(contentMode: .fit).frame(height: 40)
+            
+            ScrollView(showsIndicators: false) {
+                GeometryReader { geometry in
+                     VStack{
+                        HStack {
+                            ViewTitle("급식", sub: getDateString())
+                            Spacer()
+                            NavigationLink(destination: WeeklyMealView()) {
+                                Image("calender").resizable().aspectRatio(contentMode: .fit).frame(height: 40)
+                            }
+                        }.horizonPadding()
+                        .padding(.top, 40)
+                        HDivider().horizonPadding().offset(y: -15)
+                        VStack {
+                            SectionHeader("아침", sub: "오전 7시 30분")
+                            Text(mealAPI.getTodayMeal().breakfast)
+                                .mealMenu()
+                                .padding()
+                                .frame(width: abs(geometry.size.width-40))
+                                .background(CustomBox())
+                                .fixedSize(horizontal: false, vertical: true)
+                                
                         }
-                    }.horizonPadding()
-                    .padding(.top, 40)
-                    HDivider().horizonPadding().offset(y: -15)
-                    VStack {
-                        SectionHeader("아침", sub: "오전 7시 30분")
-                        Text(mealAPI.getTodayMeal().breakfast)
-                            .mealMenu()
-                            .padding()
-                            .frame(width: UIScreen.screenWidth-40)
-                            .background(CustomBox())
-                            .fixedSize(horizontal: false, vertical: true)
-                            
+                        VSpacer(20)
+                        VStack {
+                            SectionHeader("점심", sub: "오후 12시 50분")
+                            Text(mealAPI.getTodayMeal().lunch)
+                                .mealMenu()
+                                .padding()
+                                .frame(width: abs(geometry.size.width-40))
+                                .background(CustomBox())
+                                .fixedSize(horizontal: false, vertical: true)
+                                
+                        }
+                        VSpacer(20)
+                        VStack {
+                            SectionHeader("저녁", sub: "오후 6시 35분")
+                            Text(mealAPI.getTodayMeal().dinner)
+                                .mealMenu()
+                                .padding()
+                                .frame(width: abs(geometry.size.width-40))
+                                .background(CustomBox())
+                                .fixedSize(horizontal: false, vertical: true)
+                                
+                        }
+                        Spacer()
                     }
-                    VSpacer(20)
-                    MealItem("점심", "오후 12시 50분", mealAPI.getTodayMeal().lunch)
-                    VSpacer(20)
-                    MealItem("저녁", "오후 6시 35분", mealAPI.getTodayMeal().dinner)
-                    Spacer()
                 }
             }
             .navigationBarHidden(true)
