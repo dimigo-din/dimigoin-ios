6192bcfd9844cdc2dc0553d44cff561ed488084c
Changemin <bkm.change.min@gmail.com>
2021-03-04 09:49:24 +0900

    Fix 'meal view to have same top padding'

--- dimigoin/Views/MealView.swift
+++ dimigoin/Views/MealView.swift
@@ -59,7 +59,7 @@ struct MealView: View {
                         }.frame(width: abs(geometry.size.width-40))
                     }.frame(width: geometry.size.width)
                     VSpacer(20)
-                    HStack(spacing: 0) {
+                    HStack(alignment: .top, spacing: 0) {
                         ForEach(0..<7, id: \.self) { index in
                             VStack {
                                 SectionHeader("아침", sub: "오전 7시 30분")
